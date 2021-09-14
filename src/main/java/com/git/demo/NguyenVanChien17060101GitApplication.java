c16f9ec786998328370a2f3c1afb637ce9164a1a
NguyenVanCHien-99 <nguyenvanchien309318@gmail.com>
2021-09-14 17:42:50 +0700

    init project

---
+++ src/main/java/com/git/demo/NguyenVanChien17060101GitApplication.java
@@ -0,0 +1,13 @@
+package com.git.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenVanChien17060101GitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenVanChien17060101GitApplication.class, args);
+	}
+
+}
